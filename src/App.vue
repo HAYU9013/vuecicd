bc09027ed826dabb826e11e2dd0630b8b4c8aec2
HAYU9013 <114861748+HAYU9013@users.noreply.github.com>
2024-09-28 21:35:06 +0800

    Revert "Revert "first commit""
    This reverts commit 35640caade7fc71bbf4fcb5abbee2961f4db1991.

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+nav {
+  padding: 10px;
+  background-color: #f0f0f0;
+}
+
+nav a {
+  margin: 0 10px;
+  text-decoration: none;
+  color: #42b983;
+}
+
+nav a.router-link-active {
+  font-weight: bold;
+  color: #35495e;
+}
+</style>
